1315a8287e232ae2d2686e0f3d4bf3e5110a2aac
Dominik Auch <d.auch@dema-vertrieb.com>
2020-04-01 08:47:05 +0200

    Version 0.0.0.5

---
+++ scripts/login.php
@@ -0,0 +1,83 @@
+<?php 
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=poetry_slam', 'root', '');
+?>
+<!DOCTYPE html> 
+<html> 
+<head>
+  <title>Registrierung</title>    
+</head> 
+<body>
+ 
+<?php
+$showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+ 
+if(isset($_GET['register'])) {
+    $error = false;
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
+    $passwort2 = $_POST['passwort2'];
+  
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+        $error = true;
+    }     
+    if(strlen($passwort) == 0) {
+        echo 'Bitte ein Passwort angeben<br>';
+        $error = true;
+    }
+    if($passwort != $passwort2) {
+        echo 'Die Passwörter müssen übereinstimmen<br>';
+        $error = true;
+    }
+    
+    //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+    if(!$error) { 
+        $statement = $pdo->prepare("SELECT * FROM users WHERE username = :username");
+        $result = $statement->execute(array('username' => $username));
+        $user = $statement->fetch();
+        
+        if($user !== false) {
+            echo 'Dieser Nutzername ist bereits vergeben<br>';
+            $error = true;
+        }    
+    }
+    
+    //Keine Fehler, wir können den Nutzer registrieren
+    if(!$error) {    
+        $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+        
+        $statement = $pdo->prepare("INSERT INTO users (username, passwort) VALUES (:username, :passwort)");
+        $result = $statement->execute(array('username' => $username, 'passwort' => $passwort_hash));
+        
+        if($result) {        
+            echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+            $showFormular = false;
+        } else {
+            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+        }
+    } 
+}
+ 
+if($showFormular) {
+?>
+ 
+<form action="?register=1" method="post">
+Benutzername:<br>
+<input type="username" size="40" maxlength="250" name="username"><br><br>
+ 
+Dein Passwort:<br>
+<input type="password" size="40"  maxlength="250" name="passwort"><br>
+ 
+Passwort wiederholen:<br>
+<input type="password" size="40" maxlength="250" name="passwort2"><br><br>
+ 
+<input type="submit" value="Abschicken">
+</form>
+ 
+<?php
+} //Ende von if($showFormular)
+?>
+ 
+</body>
+</html>
